ad0b8d63eb5b650636d56291dd6e1e226da5585c
Aldwin Tablante <aldwinotablante@gmail.com>
2018-03-21 16:25:48 +0800

    Final

--- app/src/main/java/aldwin/tablante/com/finalproject/AlbumAdapter.kt
+++ app/src/main/java/aldwin/tablante/com/finalproject/AlbumAdapter.kt
@@ -1,6 +1,7 @@
 package aldwin.tablante.com.finalproject
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,13 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import java.net.URL
 
 /**
  * Created by Bobby on 21/03/2018.
  */
 class AlbumAdapter (list : ArrayList<Album>,context: Context):RecyclerView.Adapter<AlbumAdapter.MusicHolder> (){
-
+val mContext = context
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MusicHolder{
         var view = LayoutInflater.from(parent!!.context).inflate(R.layout.songlist, parent, false)
 
@@ -25,7 +27,20 @@ var data :ArrayList<Album> = list
     }
 
     override fun onBindViewHolder(holder:MusicHolder ?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val pmon = data[position]
+
+
+        val name = pmon.name
+        val artist = pmon.artist
+        val url = pmon.image
+if(url != "" || url != null) {
+    val ins = URL(url).openStream()
+
+
+    var mIcon = BitmapFactory.decodeStream(ins)
+    holder!!.imageView.setImageBitmap(mIcon)
+    holder!!.descript.setText(name + " \n" + artist)
+}
     }
 
 
--- app/src/main/java/aldwin/tablante/com/finalproject/MainActivity.kt
+++ app/src/main/java/aldwin/tablante/com/finalproject/MainActivity.kt
@@ -3,28 +3,66 @@ package aldwin.tablante.com.finalproject
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import aldwin.tablante.com.finalproject.R.id.editText
+import android.os.StrictMode
+import android.support.v7.widget.LinearLayoutManager
 import android.view.KeyEvent
+import android.view.View
+import android.widget.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
+    var prog1 : ProgressBar? = null
     var urlal = ""
+    var process1 : albumloader?=null
     var searchvalue = ""
+    var clear:Button?=null
     var searching: EditText?=null
     var albumlist : ArrayList<Album> = ArrayList()
-
-
+    var album =Album("","","")
+    var adapter :AlbumAdapter?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
         initializer()
+        var bool =1
+        button.setOnClickListener {
+            var process : albumloader = albumloader()
+            searchvalue = searching!!.text.toString()
+            if(searchvalue != "" && adapter ==null && bool ==1) {
+
+                urlal = StringUrl(searchvalue)
+
+                process.execute()
+               process1 = process
+bool =0
+            }
+            else{
+                if(process1 != null) {
+                    process1!!.cancel(true)
+                    prog1!!.visibility = View.GONE
+                    button2.isEnabled = true
+                }
+                    process1 = null
+
+                    adapter = null
+                    bool = 1
+
+
+            }
+
+        }
+        button2.setOnClickListener {
+        adapter = null
+        }
 
     }
 
@@ -34,17 +72,68 @@ class MainActivity : AppCompatActivity() {
     inner class albumloader : AsyncTask<Void,String,Void>(){
         override fun onPreExecute() {
             super.onPreExecute()
+            albumlist.clear()
+            prog1!!.visibility = View.VISIBLE
+            button2.isEnabled = false
+
         }
-        override fun doInBackground(vararg p0: Void?): Void {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+        override fun doInBackground(vararg p0: Void?): Void? {
+    try {
+        var data = StringUrl(searchvalue)
+        var limi = JSONObject(data).getJSONObject("results").getJSONObject("albummatches").getJSONArray("album")
+        var count = 0
+while(count !=  limi.length() && count<= 50) {
+
+    var data = StringUrl(searchvalue)
+    var JO = JSONObject(data).getJSONObject("results").getJSONObject("albummatches").getJSONArray("album")
+            .getJSONObject(count).getJSONArray("image").getJSONObject(3)
+
+    var JA = JSONObject(data).getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(count)
+    var image = JO.getString("#text")
+    var name = JA.getString("name")
+    var artist = JA.getString("artist")
+
+
+    albumlist.add(Album(image,name,artist))
+    adapter = AlbumAdapter(albumlist,applicationContext)
+    publishProgress()
+
+    count++
+
+}
+
+
+
+
+    }
+    catch (e:InterruptedException){
+        e.printStackTrace()
+        prog1!!.visibility = View.GONE
+        button2.isEnabled = true
+        adapter = null
+
+
+    }
+            return null
         }
 
         override fun onProgressUpdate(vararg values: String?) {
             super.onProgressUpdate(*values)
+
+
+            var layout_manager = LinearLayoutManager(applicationContext)
+            recyclerView.layoutManager = layout_manager
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
 
         override fun onPostExecute(result: Void?) {
             super.onPostExecute(result)
+            prog1!!.visibility = View.GONE
+            button2.isEnabled = true
+
         }
     }
 
@@ -52,30 +141,17 @@ class MainActivity : AppCompatActivity() {
 
     fun initializer(){
         searching = findViewById<EditText>(R.id.editText)
-        searching!!.setOnEditorActionListener(object : TextView.OnEditorActionListener {
-            override
-            fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent): Boolean {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                   searchvalue= searching!!.text.toString()
-                    urlal = StringUrl(searchvalue)
-                    var process=albumloader()
-                    process.execute()
+        prog1 = findViewById(R.id.progressBar)
 
-                    return true
-                }
-                return false
-            }
-        })
-
+        prog1!!.visibility = View.GONE
     }
 
 
     fun StringUrl ( stringer:String): String {
-        var url = URL("http://ws.audioscrobbler.com/2.0/?method=album.search&album=Destruction"+stringer.capitalize()
+        var url = URL("http://ws.audioscrobbler.com/2.0/?method=album.search&album="+stringer.capitalize()
                 +
                 "&api_key=6672f4014a94fc7befe86d6ba8ca1057&format=json")
         var httpURLConnection = url.openConnection()
-
         var inputStream = httpURLConnection.getInputStream()
         val bufferedReader = BufferedReader(InputStreamReader(inputStream))
         var line: String? = ""
